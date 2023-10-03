56254ff339b02de13eefa17d954083a0650acbe3
maedomarimanato <138412497+maedomarimanato@users.noreply.github.com>
2023-10-03 14:59:05 +0900

    Tetorisu更新

--- GameProjest/Tetorisu/APPMain.cpp
+++ GameProjest/Tetorisu/APPMain.cpp
@@ -1,23 +1,95 @@
+/**********************************
+*プログラミング実習（テトリス編）
+***********************************/ 
 #include"DxLib.h"
+#include"FreamControl.h"
+#include"SceneManager.h"
+#include"InputControl.h"
 
+/**********************************
+*マクロ定義
+**********************************/
+#define SCREEN_HEIGHT (720)
+#define SCREEN_WIDTH (1280)
+#define SCREEN_COLORBIT (32)
+#define FONT_SIZE (20)
+
+/**********************************
+*型定義
+**********************************/
+
+/*********************************
+*グローバル変数宣言
+**********************************/
+
+/*********************************
+*プロトタイプ宣言
+*********************************/
+
+/********************************
+*プログラムの開始
+*********************************/
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_
 	LPSTR lpCmdLine, _In_ int nshowCmd)
 {
+	//タイトルを変更
+	SetMainWindowText("テトリス")
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
 
+	//ウィンドウサイズの決定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
 
 	//Dxライブラリの初期化処理
 	if (DxLib_Init() == -1)
 	{
+		//エラー終了
 		return -1;
 	}
-	//入力待機
-	WaitKey();
 
+	//フレーム制御機能初期化処理
+	FreamControI_InitiaIize();
+
+	//シーン管理機能初期化処理
+	SceneManager_InitiaIize(E_TITLE);
+
+	//入力制御機能初期化処理
+	InputControI_InitiaIize();
+
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() == 0 && GetExitButton() != TRUE && ErrorCheck() ==
+		D_NORMALITY)
+	{
+		//フレーム制御機能更新処理
+		FreamControI_Uqdte();
+
+		//画面の初期化
+		CIearDrawScreen();
+
+		//入力制御機能更新処理
+		InputControl_Update();
+
+		//シーン管理機能更新処理
+		SceneManager_Update();
+
+		//シーン管理機能描画処理
+		SceneManager_Draw();
+
+		//裏画面の内容を表画面に反映
+		ScreenFIip();
+	}
 
 	//Dxライブラリ使用の終了処理
 	DxLib_End();
+
+	//ソフトの終了
 	return 0;
 }
